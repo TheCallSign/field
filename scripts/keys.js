9a8ec28f8ae753dc8c2a4d6b3ec2b781ae679f98
jamie <16000925@vcconnect.co.za>
2018-02-26 16:33:26 +0200

    bare bones

--- scripts/keys.js
+++ scripts/keys.js
@@ -1,14 +1,17 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-
+    var currentPortfolioItem = document.getElementsByClassName('portfolio-item')[0];
 
     $('#keys li').hover(function(){
         var index = $(this).index();
-
         var portfolioItem = document.getElementsByClassName('portfolio-item')[index];
-        $(portfolioItem).fadeTo(1000,1);
 
+        currentPortfolioItem.style.zIndex = "-1";
+        portfolioItem.style.zIndex = "1";
 
+        $(portfolioItem).fadeTo(700,1);
+        $(currentPortfolioItem).fadeTo(100,0);
+        currentPortfolioItem = portfolioItem;
 
     });
 
