9e9b903f8d8968f3c647dbd8c146f2bbd2568c13
jamie <16000925@vcconnect.co.za>
2018-02-27 12:59:26 +0200

    key background and video change

--- scripts/keys.js
+++ scripts/keys.js
@@ -2,10 +2,36 @@ $( document ).ready(function() {
     console.log( "ready!" );
     var currentPortfolioItem = document.getElementsByClassName('portfolio-item')[0];
 
+    // $('#keys li').hover(function(){
+    //     var index = $(this).index();
+    //     var portfolioItem = document.getElementsByClassName('portfolio-item')[index];
+
+    //     currentPortfolioItem.style.zIndex = "-1";
+    //     portfolioItem.style.zIndex = "1";
+
+    //     $(portfolioItem).fadeTo(700,1);
+    //     $(currentPortfolioItem).fadeTo(100,0);
+    //     currentPortfolioItem = portfolioItem;
+    
+    //     // background-image: url("../media/images/Portfolio-TitleHover(P5).png");
+
+    //     var backgroundImage = 'url("media/images/Portfolio-TitleHover(P' + (index + 1) +').png")';
+    //     $(this).css('background-image', backgroundImage);
+    //     // $(this).css('background-color', 'red');
+        
+    //     console.log('setting ' + backgroundImage);
+
+    // });
+
     $('#keys li').hover(function(){
         var index = $(this).index();
-        var portfolioItem = document.getElementsByClassName('portfolio-item')[index];
 
+        //display key background color
+        var backgroundImage = 'url("media/images/Portfolio-TitleHover(P' + (index + 1) +').png")';
+        $(this).css('background-image', backgroundImage);
+
+        //change video
+        var portfolioItem = document.getElementsByClassName('portfolio-item')[index];
         currentPortfolioItem.style.zIndex = "-1";
         portfolioItem.style.zIndex = "1";
 
@@ -13,7 +39,16 @@ $( document ).ready(function() {
         $(currentPortfolioItem).fadeTo(100,0);
         currentPortfolioItem = portfolioItem;
 
+
+               
+    }, function(){
+        $(this).css('background-image', '');
+        
     });
 
+    // function displayBackground(element){
+    //     $(element).css('background-image', );
+    // }
 });
+
 
