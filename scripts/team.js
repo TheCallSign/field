4ba996e63bc4eafc2b471d28a5d7c5d0c2d26991
jamie <16000925@vcconnect.co.za>
2018-03-05 23:52:16 +0200

    team member carousel controls

--- scripts/team.js
+++ scripts/team.js
@@ -1,25 +1,19 @@
+	var currentIndex = 0;
+	var teamMembers = $('.team-member');
+	var numTeamMembers = teamMembers.length;
 $( document ).ready(function() {
 
 //swap team members
-	var currentIndex = 0;
-	var items = $('.team-member');
-	var itemCount = items.length;
 	var refreshTime = 3000;
 
 	var paused = false;
 
-	function cycleItems(){
-		var item = $('.team-member').eq(currentIndex);
-		items.hide();
-		item.css('display','inline-block');
-	}
-
 	var autoSlide = setInterval(function(){
-		currentIndex += 1;
-		if (currentIndex > itemCount - 1){
-			currentIndex = 0;
-		}
 		if(!paused){
+			currentIndex += 1;
+			if (currentIndex > numTeamMembers - 1){
+				currentIndex = 0;
+			}
 			cycleItems();
 		}
 	},refreshTime);
@@ -33,6 +27,22 @@ $( document ).ready(function() {
 
 });
 
-function scrollTeam(){
+function cycleItems(){
+	var item = $('.team-member').eq(currentIndex);
+	teamMembers.hide();
+	item.css('display','inline-block');
+}
 
+
+function scrollTeam(direction){
+	//check boundries
+	if(currentIndex + direction >= 0 && currentIndex + direction < numTeamMembers){
+		console.log('currentIndex : ' + currentIndex);
+		currentIndex += direction;
+		console.log('scrolling to : ' + currentIndex);
+		cycleItems();
+	} else {
+		console.log('I cant go that way anymore.' + 'cIndex: ' + currentIndex);
+		
+	}
 }
