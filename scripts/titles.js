749d1e64e411887644329195d7d5166f0b1ecde9
geimaj <16000925@vcconnect.co.za>
2018-05-20 16:38:55 +0200

    start title animations

--- scripts/titles.js
+++ scripts/titles.js
@@ -1,46 +1,58 @@
 var titles = document.getElementById("titles");
-var titlesScrollLimit = 25;
-var titlesScrollAmmount = 3;
+var titlesScrollOffset = 25;
+var titlesScrollAmmount = 0.5;
+var scrollInterval = 15;
 var scrollCheck;
 var onTitles = false;
 var mouseX;
+var titleItems = document.getElementsByClassName("title-item");
+var animationCheck;
 
-titles.onmouseenter = function(event) {
+//keep track of mouse x
+document.addEventListener("mousemove", function(event){
+    mouseX = event.pageX;
+});
+
+titles.onmouseenter = function() {
      onTitles = true;
+     animationCheck = window.setInterval(checkAnimation, 10);
 }
 
 titles.onmouseleave = function() {
     onTitles = false;   
+    clearInterval(animationCheck);
 }
 
-document.addEventListener("mousemove", function(event){
-    mouseX = event.pageX;
-})
-
+//scroll check
 var scroll = 0;
-var maxScroll = 10;
+var maxScroll = 30;
 scrollCheck = window.setInterval(function(){
     
     if(onTitles){
-        //get mouse pos
+        //get mouse pos as %
         var w = titles.offsetWidth;
         var pos = mouseX / w * 100;
 
-        if(titlesScrollLimit >= pos ){ //check left
+        if(titlesScrollOffset >= pos ){ //check left third
                 if(Math.abs(scroll-titlesScrollAmmount) < maxScroll)
                     scroll -= titlesScrollAmmount;
-        } else if (pos >= (100 - titlesScrollLimit)){ //check right
+        } else if (pos >= (100 - titlesScrollOffset)){ //check right third
             if(scroll+titlesScrollAmmount < maxScroll)   
                 scroll += titlesScrollAmmount;
         } else {
             scroll = 0;
         }
 
-        console.log('scroll: ' + scroll);
-
         titles.scrollLeft += scroll;
-
     } else {
         scroll = 0;
     }
-}, 25);
+}, scrollInterval);
+
+//animate hovered item
+$('.title-item').hover(function(){
+    $(this).css('background', 'gray');
+}, function(){
+    $(this).css('background', 'none');
+    
+});
