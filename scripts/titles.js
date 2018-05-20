7b2f3cf12f660ba48a9476b4e2b50685cc0c3133
geimaj <16000925@vcconnect.co.za>
2018-05-20 21:27:03 +0200

    map scroll to mouse pos

--- scripts/titles.js
+++ scripts/titles.js
@@ -1,58 +1,22 @@
-var titles = document.getElementById("titles");
-var titlesScrollOffset = 25;
-var titlesScrollAmmount = 0.5;
-var scrollInterval = 15;
-var scrollCheck;
-var onTitles = false;
-var mouseX;
-var titleItems = document.getElementsByClassName("title-item");
-var animationCheck;
+var $titles = $("#titles"),
+      titlesWidth = $titles.outerWidth(true),
+      titlesScrollWidth = $titles[0].scrollWidth,
+      wDiff = (titlesScrollWidth / titlesWidth) - 1, // widths difference ratio
+      mPadd = 60, // Mousemove Padding
+      damp = 20, // Mousemove response softness
+      mX = 0, // Real mouse position
+      mX2 = 0, // Modified mouse position
+      posX = 0,
+      mmAA = titlesWidth - (mPadd * 2), // The mousemove available area
+      mmAAr = (titlesWidth / mmAA); // get available mousemove fidderence ratio
 
-//keep track of mouse x
-document.addEventListener("mousemove", function(event){
-    mouseX = event.pageX;
-});
+    $titles.mousemove(function(e) {
+      mX = e.pageX - $(this).offset().left;
+      mX2 = Math.min(Math.max(0, mX - mPadd), mmAA) * mmAAr;
+    });
 
-titles.onmouseenter = function() {
-     onTitles = true;
-     animationCheck = window.setInterval(checkAnimation, 10);
-}
+    setInterval(function() {
+      posX += (mX2 - posX) / damp; // zeno's paradox equation "catching delay"	
+      $titles.scrollLeft(posX * wDiff);
+    }, 10);
 
-titles.onmouseleave = function() {
-    onTitles = false;   
-    clearInterval(animationCheck);
-}
-
-//scroll check
-var scroll = 0;
-var maxScroll = 30;
-scrollCheck = window.setInterval(function(){
-    
-    if(onTitles){
-        //get mouse pos as %
-        var w = titles.offsetWidth;
-        var pos = mouseX / w * 100;
-
-        if(titlesScrollOffset >= pos ){ //check left third
-                if(Math.abs(scroll-titlesScrollAmmount) < maxScroll)
-                    scroll -= titlesScrollAmmount;
-        } else if (pos >= (100 - titlesScrollOffset)){ //check right third
-            if(scroll+titlesScrollAmmount < maxScroll)   
-                scroll += titlesScrollAmmount;
-        } else {
-            scroll = 0;
-        }
-
-        titles.scrollLeft += scroll;
-    } else {
-        scroll = 0;
-    }
-}, scrollInterval);
-
-//animate hovered item
-$('.title-item').hover(function(){
-    $(this).css('background', 'gray');
-}, function(){
-    $(this).css('background', 'none');
-    
-});
